647981ea7230649a13d17f2eb23d432d0b7ead5a
navin1272000 <navin1272000@gmail.com>
2025-11-30 11:34:12 +0000

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { TaskManager } from './pages/task-manager/task-manager';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:Home},
+    {path:'task',component:TaskManager}
+];
---
+++ src/app/pages/task-manager/task-manager.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-task-manager',
+  imports: [RouterLink],
+  templateUrl: './task-manager.html',
+  styleUrl: './task-manager.css',
+})
+export class TaskManager {
+
+}
